e0513c248a794a4e3c1db5ca046336065821a44f
Noe-Martinez-Flores <20203tn035@utez.edu.mx>
2022-05-01 20:06:32 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 
-import {saludar} from './js/componentes';
 import './css/styles.css';
+import {Todo, TodoList} from './classes'
+import { crearToDoHtml } from './js/componentes';
 
 
-const nombre = 'Noé Martínez';
-saludar(nombre); 
+
+export const todoList = new TodoList();
+
+todoList.todos.forEach(crearToDoHtml);
+
+console.log(todoList.todos);
